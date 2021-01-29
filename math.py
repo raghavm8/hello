365d24445749b7475a84ca0466ea0a689fa68015
raghavm8 <53058548+raghavm8@users.noreply.github.com>
2021-01-29 15:42:50 +0530

    math.py added

---
+++ math.py
@@ -0,0 +1,17 @@
+# This is a sample function
+'''
+dsfgdgfd
+dgffdg
+dgfg
+'''
+def add(x,y):
+    pass
+def subtract(x,y):
+    pass
+def multiply(x,y):
+    pass
+def divide(x,y):
+    pass
+if __name__ == '__main__':
+    print(foo(3, 4))
+    print(foo(7, 9))
